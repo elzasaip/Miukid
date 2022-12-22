fca6942d15cf663a4b1a59d8e4c14807f146fa0c
ElzaSaipova <elza8901@gmail.com>
2022-12-22 20:39:54 +0400

    to get text price img

--- script.js
+++
@@ -1,4 +0,0 @@
-let myName="Elza";
-let myAge=33;
-var year=1989;
-console.log(myName,myAge,year);
---
+++ scripts/add_to_cart.js
@@ -0,0 +1,8 @@
+let title=document.getElementsByClassName("title");
+console.log(title[0].innerText);
+
+let price=document.getElementsByClassName("price");
+console.log(price[0].outerText);
+
+let image = document.getElementsByTagName("img");
+console.log (image[1].src);
