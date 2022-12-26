8105f0814788caa79dd47fb64a7b74637ea6175f
ElzaSaipova <elza8901@gmail.com>
2022-12-26 19:58:22 +0400

    get area circle and squere

--- scripts/function.js
+++ scripts/function.js
@@ -1,18 +1,54 @@
+// // 30 DZ
+// let radius = Number(prompt('Введите радиус:', '')); 
+// function calculate(){
+//     let pi = 3.14;
+//     let circumference = 2*pi*radius;
+//     alert("длина окружности: " + circumference);
+// }
+// calculate();
+
+// function checkMin(a,b){
+//     if (a<b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
+// checkMin(34256,7640);
+
+// 31 DZ
+
+// Сделайте функцию которая возводить в степень числа вводимые пользователем
+let num = Number(prompt('Введите число:', '')); 
+let degree = Number(prompt('Введите число степени:', ''));
+function calcDegree(){
+    let result = num**degree;
+    alert("степень числа: " + result);
+}
+calcDegree();
+
+// Сделайте функцию которая находит площадь круга, радиус вводить пользователь.
 let radius = Number(prompt('Введите радиус:', '')); 
 function calculate(){
     let pi = 3.14;
-    let circumference = 2*pi*radius;
-    alert("длина окружности: " + circumference);
+    let circleArea = pi*radius**2;
+    alert("площадь круга: " + circleArea);
 }
 calculate();
+ 
+// Сделайте функцию которая находит площадь прямоугольника, длину и ширину вводить пользователь. 
 
-function checkMin(a,b){
-    if (a<b){
-        return a;
-    }
-    else{
-        return b;
-    }
+// function calcArea(a,b){
+//     let S = a*b;
+//     console.log("площадь прямоугольника: " + S);
+// }
+// calcArea(6,7);
+
+let a = Number(prompt('Введите длину прямоугольника:', '')); 
+let b = Number(prompt('Введите ширину прямоугольника:', ''));
+function calcArea(){
+    let S = a*b;
+    alert("площадь прямоугольника: " + S);
 }
-checkMin(34256,7640);
-
+calcArea();
