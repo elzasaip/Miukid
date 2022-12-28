dcd6ef60117c7753170b710fa825ae83be84f21f
ElzaSaipova <elza8901@gmail.com>
2022-12-28 11:26:27 +0400

    method numbers

--- scripts/function.js
+++ scripts/function.js
@@ -20,22 +20,22 @@
 // 31 DZ
 
 // Сделайте функцию которая возводить в степень числа вводимые пользователем
-let num = Number(prompt('Введите число:', '')); 
-let degree = Number(prompt('Введите число степени:', ''));
-function calcDegree(){
-    let result = num**degree;
-    alert("степень числа: " + result);
-}
-calcDegree();
+// let num = Number(prompt('Введите число:', '')); 
+// let degree = Number(prompt('Введите число степени:', ''));
+// function calcDegree(){
+//     let result = num**degree;
+//     alert("степень числа: " + result);
+// }
+// calcDegree();
 
 // Сделайте функцию которая находит площадь круга, радиус вводить пользователь.
-let radius = Number(prompt('Введите радиус:', '')); 
-function calculate(){
-    let pi = 3.14;
-    let circleArea = pi*radius**2;
-    alert("площадь круга: " + circleArea);
-}
-calculate();
+// let radius = Number(prompt('Введите радиус:', '')); 
+// function calculate(){
+//     let pi = 3.14;
+//     let circleArea = pi*radius**2;
+//     alert("площадь круга: " + circleArea);
+// }
+// calculate();
  
 // Сделайте функцию которая находит площадь прямоугольника, длину и ширину вводить пользователь. 
 
@@ -45,10 +45,36 @@ calculate();
 // }
 // calcArea(6,7);
 
-let a = Number(prompt('Введите длину прямоугольника:', '')); 
-let b = Number(prompt('Введите ширину прямоугольника:', ''));
-function calcArea(){
-    let S = a*b;
-    alert("площадь прямоугольника: " + S);
+// let a = Number(prompt('Введите длину прямоугольника:', '')); 
+// let b = Number(prompt('Введите ширину прямоугольника:', ''));
+// function calcArea(){
+//     let S = a*b;
+//     alert("площадь прямоугольника: " + S);
+// }
+// calcArea();
+
+// 32 DZ
+// сделайте функцию которая суммируем два числа с плавающей точкой
+// пример: если пользователь  ввел 1.2, 1.2 вывод должен быть 2. 
+// let a = parseInt(prompt('Введите первое число:', '')); 
+// let b = parseInt(prompt('Введите второе число:', ''));
+// function calcSum(){
+//     let Summ = a+b;
+//     alert("сумма чисел: " + Summ);
+// }
+// calcSum();
+
+// пример: если пользователь  ввел 2.5, 2.3 вывод должен быть 5
+// пример: если пользователь  ввел 2,5, 2.7 вывод должен быть 5. 
+// пример: если пользователь  ввел 2,5, 3,2 вывод должен быть 6.
+let c = Number(prompt('Введите первое число:', '')); 
+let d = Number(prompt('Введите второе число:', ''));
+function calcSumm(){
+    let Summ = c+d;
+    alert(Summ.toFixed(0));
 }
-calcArea();
+calcSumm();
+
+
+
+
