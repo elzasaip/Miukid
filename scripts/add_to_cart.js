ec4abf7db8392e660eb532507b5bb23dd360f491
ElzaSaipova <elza8901@gmail.com>
2023-01-02 23:28:35 +0400

    add cart, creat element

--- scripts/add_to_cart.js
+++ scripts/add_to_cart.js
@@ -1,66 +1,42 @@
-// ДЗ 28
-// let title=document.getElementsByClassName("title");
-// console.log(title[0].innerText);
 
-// let price=document.getElementsByClassName("price");
-// console.log(price[0].outerText);
+// 33 Urok Добавление товара в корзину
+let elements = document.getElementsByClassName("add_cart");
 
-// let image = document.getElementsByTagName("img");
-// console.log (image[1].src);
+let myFunction = function(event){
+    event.preventDefault();
+    let title = this.parentNode.parentNode.parentNode.querySelectorAll(".title");
+    let price = this.parentNode.parentNode.parentNode.querySelectorAll('.price > span');
+    let delPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price > del');
+    let image= this.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".img > img");
 
-// ДЗ 29
-//        1)
-// let title=document.querySelectorAll(".body_product > .title");
-// for ( let text of title){
-//     console.log(text.innerText);
-// }
+    let create_li= document.createElement('li');
 
-// let price=document.getElementsByClassName("price");
-// for( var value of price){
-//     console.log(value.innerText);
-// }
+    console.log(title[0].innerText);
+    console.log(price[0].innerText);
+    console.log(delPrice[0].innerText);
+    console.log(image[0].src);
 
-// let image=document.querySelectorAll(".img > img");
-// for(var picture of image){
-//     console.log(picture.src);
-// }
+    create_li.innerHTML ='<div class="img">\
+                            <img src="'+image[0].src+'">\
+                        </div>\
+                        <div class="body_product">\
+                            <p class="title">'+title[0].innerText+'</p>\
+                            <div class="price">\
+                                <span>'+price[0].innerText+'</span>\
+                                <del>'+delPrice[0].innerText+'</del>\
+                            </div>\
+                        </div>\
+                        <div class="del_icon">\
+                            <i class="fa-solid fa-trash-can"></i>\
+                        </div>';
 
-//         2)
-//   add text
-var elements = document.getElementsByClassName("add_cart");
-
-var myFunction = function(){
-    var attribute = this.parentNode.parentNode.parentNode.querySelectorAll(".title");
-    console.log(attribute[0].innerText);
+    let block_myCart = document.querySelector('.bag.dropdown_menu ul');
+    block_myCart.appendChild(create_li);
+    alert("Товар успешно добавлен в корзину.");
 };
-for (var i = 0; i < elements.length; i++){
+
+// event.preventDefault();  ----игнорировать "#"href
+for (var i=0; i<elements.length; i++){
     elements[i].addEventListener('click', myFunction, false);
 }
 
-//    add price
-var addPrice = function(){
-    var attrib = this.parentNode.parentNode.parentNode.querySelectorAll(".price");
-    console.log(attrib[0].innerText);
-};
-for (var i = 0; i < elements.length; i++){
-    elements[i].addEventListener('click', addPrice, false);
-}
-
-//    add Picture
-var addImage = function(){
-    var picture = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".img > img");
-    console.log(picture[0].src);
-};
-for (var i = 0; i < elements.length; i++){
-    elements[i].addEventListener('click', addImage, false);
-}
-
-const productName = document.querySelectorAll('.body_product > .title');
-const textContent = productName.innerHTML;
-productName.innerHTML = 'Overalls For Kids';
-console.log(productName.innerHTML);
-
-var priceElem = document.querySelectorAll('.price > span');
-var priceElemContent = priceElem.innerHTML;
-priceElem.innerHTML = '&15';
-console.log(priceElem.innerHTML);
