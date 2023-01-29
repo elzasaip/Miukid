f09a9569eeb9fc2975f72f9ed6b77b42cc1fcf97
ElzaSaipova <elza8901@gmail.com>
2023-01-29 22:00:19 +0400

    delete prod & get total price

--- scripts/add_to_cart.js
+++ scripts/add_to_cart.js
@@ -5,24 +5,21 @@ let elements = document.getElementsByClassName("add_cart");
 let myFunction = function(event){
     event.preventDefault();
     let title = this.parentNode.parentNode.parentNode.querySelectorAll(".title");
-    let price = this.parentNode.parentNode.parentNode.querySelectorAll('.price > span');
+    let price = this.parentNode.parentNode.parentNode.querySelectorAll('.price .prod_price');
     let delPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price > del');
     let image= this.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".img > img");
 
     let create_li= document.createElement('li');
+    create_li.classList.add("bag__item");
 
-    console.log(title[0].innerText);
-    console.log(price[0].innerText);
-    console.log(delPrice[0].innerText);
-    console.log(image[0].src);
-
-    create_li.innerHTML ='<div class="img">\
+    create_li.innerHTML ='<div class="bag__img">\
                             <img src="'+image[0].src+'">\
                         </div>\
-                        <div class="body_product">\
-                            <p class="title">'+title[0].innerText+'</p>\
-                            <div class="price">\
-                                <span>'+price[0].innerText+'</span>\
+                        <div class="bag__info">\
+                            <p class="info__title">'+title[0].innerText+'</p>\
+                            <div class="info__price">\
+                                <span class="currency">$</span>\
+                                <span class="prod_price">'+price[0].innerText+'</span>\
                                 <del>'+delPrice[0].innerText+'</del>\
                             </div>\
                         </div>\
@@ -33,6 +30,12 @@ let myFunction = function(event){
     let block_myCart = document.querySelector('.bag.dropdown_menu ul');
     block_myCart.appendChild(create_li);
     alert("Товар успешно добавлен в корзину.");
+
+    // Удаление товара
+	deleteProduct();
+
+		//получить общее количество суммы товаров в корзине
+    getTotalPrice();
 };
 
 // event.preventDefault();  ----игнорировать "#"href
@@ -40,3 +43,33 @@ for (var i=0; i<elements.length; i++){
     elements[i].addEventListener('click', myFunction, false);
 }
 
+
+function deleteProduct(){
+    let click_trash = document.querySelectorAll(".del_icon");
+
+    for (var i = 0; i < click_trash.length; i++) {
+        click_trash[i].addEventListener('click', delFromCart, false);
+    }
+
+    function delFromCart(event){
+        event.preventDefault();
+        this.parentNode.parentNode.remove();
+
+        getTotalPrice();
+    }
+}
+
+
+function getTotalPrice() {
+    let summPrice = document.querySelectorAll(".info__price .prod_price");
+
+    let total_price = 0;
+
+    for (var i = 0; i < summPrice.length; i++) {
+        total_price = total_price + (+summPrice[i].textContent);
+    }
+
+    document.getElementsByClassName('total_cart')[0].innerHTML = `$ ${total_price}`;
+    document.querySelectorAll('.total_price .get_price')[0].innerHTML = `$ ${total_price}`;
+    
+}
