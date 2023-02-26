41a0aa44827740caa4a73d5a7d8842f7f35f2dfe
ElzaSaipova <elza8901@gmail.com>
2023-02-26 18:46:49 +0400

    reinstall npm, axios

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 (function(){
     'use strict';
-      fetch("db.json")
-      .then(data => data.json())
-      .then(res  => console.log(res));
+      // fetch("http://localhost:3000/products")
+      // .then(data => data.json())
+      // .then(res  => console.log(res));
+
+      axios.get("http://localhost:3000/products")
+      .then(data  => console.log(data));
+
   
 }());
